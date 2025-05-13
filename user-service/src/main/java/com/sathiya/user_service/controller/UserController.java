aa5e79ca6055ccc7f94cebd20f58680aec798dfc
Sathiya Narayanan <sathiyanarayanan188@gmail.com>
2025-05-13 22:41:13 +0530

    initial commit

---
+++ user-service/src/main/java/com/sathiya/user_service/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.sathiya.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Hello from User Service";
+    }
+}
